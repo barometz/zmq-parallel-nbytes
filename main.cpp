28a1a316fe7b43310bd4ef70d0e2dc3ff12721ec
Dominic van Berkel <dominic.van.berkel@prodrive-technologies.com>
2020-05-06 17:08:59 +0200

    Smallest failing case

--- main.cpp
+++ main.cpp
@@ -6,28 +6,21 @@
 
 int main(int, char**)
 {
-  const int cycles = 500;
+  const int cycles = 200;
+  const int task_count = 6;
 
   std::list<std::future<void>> tasks;
-  for (int task_number = 1; task_number < 9; task_number++)
+  for (int task = 0; task < task_count; task++)
   {
     tasks.emplace_back(std::async(std::launch::async, [&]
     {
       for (int i = 0; i < cycles; i++)
       {
-        const std::string address = "udp://127.0.0." + std::to_string(task_number) + ";255.0.0.1:5599";
+        const std::string address = "udp://127.0.0." + std::to_string(task + 1) + ";255.0.0.1:5599";
 
         void* ctx = zmq_ctx_new();
-        auto dish = zmq_socket(ctx, ZMQ_DISH);
-        auto radio = zmq_socket(ctx, ZMQ_RADIO);
-
-        zmq_bind(dish, address.c_str());
-        zmq_connect(radio, address.c_str());
-
-        zmq_join(dish, "swp");
-
-        zmq_close(dish);
-        zmq_close(radio);
+        auto socket = zmq_socket(ctx, ZMQ_RADIO);
+        zmq_close(socket);
         zmq_ctx_term(ctx);
       }
     }));
